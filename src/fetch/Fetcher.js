0698a0c9a91ae8ebccf777eb639a601a087083d6
Khinenw <deu07115@gmail.com>
2022-04-14 22:46:05 +0900

    chore(fetcher): Add default axios headers

--- src/fetch/Fetcher.js
+++ src/fetch/Fetcher.js
@@ -43,6 +43,8 @@ export default class Fetcher {
 			globalAxios.defaults,
 			{
 				headers: {
+					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+					'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
 					'User-Agent': this.options.request.UserAgent || rescrap.defaultUserAgent
 				},
 
